2ab401d2f078d533bcafd136b35036046c58a37d
feliixwww <felix@soul-werner.de>
2024-01-18 15:50:49 +0100

    7

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,21 @@
 const myHeading = document.querySelector("h1");
-myHeading.textContent = "Bob";
+myHeading.textContent = "Bob!";
 
-myHeading.textContent = "Bob" + " Joooo es ist Bob";
+myHeading.textContent = "Bob!" + " Joooo es ist Bob";
 
+
+
+const myImage = document.querySelector("#cat");
+const myImage2 = document.querySelector("#hand");
+const anzahl = document.querySelector("#anzahl")
+let gestreichelt = 0
+
+myImage.onclick = () => {
+  if (myImage2.style.visibility === "hidden") {
+    myImage2.style.visibility = "visible";
+    gestreichelt += 1
+    anzahl.innerHTML = gestreichelt
+  } else {
+    myImage2.style.visibility = "hidden";
+  }
+};
